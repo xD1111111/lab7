754e887db3bfc81124fe4f300fb19fc51b3b0eb9
xd <artem082022@gmail.com>
2026-05-10 23:52:49 +0300

    feat: add Observable implementation

--- ReactiveComm.js
+++ ReactiveComm.js
@@ -26,4 +26,60 @@ class MessageBus extends EventEmitter {
   }
 }
 
-module.exports = { MessageBus };
+class Observable {
+  constructor(subscribeFn) {
+    this._subscribeFn = subscribeFn;
+  }
+
+  subscribe(observer) {
+    if (typeof observer === 'function') {
+      observer = { next: observer };
+    }
+
+    const safeObserver = {
+      next: observer.next || (() => {}),
+      error: observer.error || ((e) => { throw e; }),
+      complete: observer.complete || (() => {}),
+    };
+
+    let unsubscribed = false;
+    let cleanup;
+
+    const subscription = {
+      unsubscribe() {
+        unsubscribed = true;
+        if (cleanup) cleanup();
+      }
+    };
+
+    const wrappedObserver = {
+      next(value) { if (!unsubscribed) safeObserver.next(value); },
+      error(err)  { if (!unsubscribed) safeObserver.error(err); },
+      complete()  { if (!unsubscribed) safeObserver.complete(); },
+    };
+
+    cleanup = this._subscribeFn(wrappedObserver);
+    return subscription;
+  }
+
+  pipe(...operators) {
+    return operators.reduce((obs, op) => op(obs), this);
+  }
+
+  static fromEvent(emitter, event) {
+    return new Observable((observer) => {
+      const handler = (data) => observer.next(data);
+      emitter.on(event, handler);
+      return () => emitter.off(event, handler);
+    });
+  }
+
+  static of(...values) {
+    return new Observable((observer) => {
+      values.forEach((v) => observer.next(v));
+      observer.complete();
+    });
+  }
+}
+
+module.exports = { MessageBus, Observable };
